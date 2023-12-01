2ecd598c082d8e658b7b517b3f785dbc4f6e1daf
ADMIN <ADMIN@BHARANIDHARAN>
2023-12-01 14:40:41 +0530

    first1

---
+++ Firstpushpriject/src/firstpackpush/firstcommitpush.java
@@ -0,0 +1,13 @@
+package firstpackpush;
+
+public class firstcommitpush {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hellow world");
+		System.out.println("happy life");
+
+	}
+
+}
